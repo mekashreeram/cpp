07429409d6eeb916e3578bf6d8d9eff2bc4cff43
mekashreeram <21131a1223@gvpce.ac.in>
2023-02-14 09:06:23 +0530

    Add First Week Program

---
+++ One.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int add(int, int);
+double add(int, double);
+double add(double, int);
+int add(double, double);
+
+int main()
+{
+    cout << add(2, 3) << " : int + int" << endl;
+    cout << add(2, 3.14) << " : int + double" << endl;
+    cout << add(2.45, 3) << " : double + int" << endl;
+    cout << add(2.45, 3.14) << " : double + double" << endl;
+}
+
+int add(int a, int b)
+{
+    return a + b;
+}
+
+double add(int a, double b)
+{
+    return a + b;
+}
+
+double add(double a, int b)
+{
+    return a + b;
+}
+
+int add(double a, double b)
+{
+    return a + b;
+}
