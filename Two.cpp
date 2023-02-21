8ab63874654307fbbc306ef22ce123ca9ddef687
mekashreeram <21131a1223@gvpce.ac.in>
2023-02-21 08:25:14 +0530

    Add Unary Operator Overloading.

--- Two.cpp
+++ Two.cpp
@@ -11,35 +11,31 @@ class Complex
             real = r;
             img = i;
         }
-        Complex operator +(const Complex obj)
+
+        // Binary Operator Overloading:
+
+        // friend Complex operator +(Complex, Complex);   //using friend function
+
+        Complex operator +(const Complex obj)             //using in class overloading
         {
             return Complex(real + obj.real, img + obj.img);
         }
+
+
+        // Unary Operator Overloading:
+
+        Complex operator -()
+        {
+            return Complex(-(real), -(img));
+        }
+
         void printNum()
         {
             cout << real << " + i" << "(" << img << ")";
         }
 };
 
-// using friend function
-
-// class Complex
-// {
-//     public:
-//         int real, img;
-//         Complex(int r, int i)
-//         {
-//             real = r;
-//             img = i;
-//         }
-//         friend Complex operator +(Complex, Complex);
-//         void printNum()
-//         {
-//             cout << real << " + i" << "(" << img << ")";
-//         }
-// };
-
-// Complex operator +(Complex c1, Complex c2)
+// Complex operator +(Complex c1, Complex c2)                 //using friend function
 // {
 //     return Complex(c1.real + c2.real, c1.img + c2.img);
 // }
@@ -49,6 +45,13 @@ int main()
     Complex a(3, 4);
     Complex b(14, -12);
     Complex c = a + b;
+    cout << "Binary Operator Overloading (Addition) : ";
     c.printNum();
+    cout << endl << "Unary Operator Overloading (Unary minus) : ";
+    Complex d = -a;
+    cout << "d = -a where a = ";
+    a.printNum();
+    cout << endl << "d = ";
+    d.printNum();
     cout << endl;
 }
